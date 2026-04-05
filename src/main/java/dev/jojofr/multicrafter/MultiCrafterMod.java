e888384387eaa2396d2cbeb0872d8af1a2e17872
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-04-05 21:18:33 +0200

    Rename main class to MultiCrafterMod

---
+++ src/main/java/dev/jojofr/multicrafter/MultiCrafterMod.java
@@ -0,0 +1,11 @@
+package dev.jojofr.multicrafter;
+
+import arc.util.Log;
+import mindustry.mod.Mod;
+
+public class MultiCrafterMod extends Mod{
+
+    public MultiCrafterMod() {
+        Log.info("[MultiCrafter] Library successfully loaded!");
+    }
+}
