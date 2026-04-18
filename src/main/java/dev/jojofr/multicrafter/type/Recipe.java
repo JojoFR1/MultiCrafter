dcb051391ccd5f85e622c2e13a8f3d4f19d50970
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-04-18 02:22:56 +0200

    add builder setter for Recipe

--- src/main/java/dev/jojofr/multicrafter/type/Recipe.java
+++ src/main/java/dev/jojofr/multicrafter/type/Recipe.java
@@ -8,7 +8,7 @@ import mindustry.entities.Effect;
 public class Recipe extends UnlockableContent {
     public final IOEntry input, output;
     
-    public float craftTime = 80;
+    public float craftTime;
     public Effect craftEffect = Fx.none;
     public Effect updateEffect = Fx.none;
     public float updateEffectChance = 0.04f;
@@ -18,9 +18,38 @@ public class Recipe extends UnlockableContent {
     public Recipe(String name) { this(name, new IOEntry(), new IOEntry()); }
     public Recipe(String name, IOEntry input) { this(name, input, new IOEntry()); }
     public Recipe(String name, IOEntry input, IOEntry output) {
+        this(name, input, output, 80);
+    }
+    public Recipe(String name, IOEntry input, IOEntry output, float craftTime) {
         super(name);
         this.input = input;
         this.output = output;
+        this.craftTime = craftTime;
+    }
+    
+    public Recipe withCraftTime(float craftTime) {
+        this.craftTime = craftTime;
+        return this;
+    }
+    
+    public Recipe withCraftEffect(Effect craftEffect) {
+        this.craftEffect = craftEffect;
+        return this;
+    }
+    
+    public Recipe withUpdateEffect(Effect updateEffect) {
+        return withUpdateEffect(updateEffect, 0.04f, 4f);
+    }
+    public Recipe withUpdateEffect(Effect updateEffect, float chance, float spread) {
+        this.updateEffect = updateEffect;
+        this.updateEffectChance = chance;
+        this.updateEffectSpread = spread;
+        return this;
+    }
+    
+    public Recipe withWarmupSpeed(float speed) {
+        this.warmupSpeed = speed;
+        return this;
     }
     
     @Override
