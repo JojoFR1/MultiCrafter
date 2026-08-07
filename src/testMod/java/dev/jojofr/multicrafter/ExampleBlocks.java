898bc722e4eecf14c88e6ca89171de4ce914b25e
Jojo <110781915+JojoFR1@users.noreply.github.com>
2026-08-07 18:56:48 +0200

    update Java example file

--- src/testMod/java/dev/jojofr/multicrafter/ExampleBlocks.java
+++ src/testMod/java/dev/jojofr/multicrafter/ExampleBlocks.java
@@ -27,8 +27,7 @@ public class ExampleBlocks {
             liquidCapacity = 50;
             
             drawer = new DrawDefault();
-            
-            // A special drawer, 'DrawRecipe', can be used to draw the recipes drawer at its place. You CANNOT have the two versions (above and below) at the same time, it is just a choice.
+            // A special drawer, 'DrawRecipe', can be used to draw the recipes drawer at its place. The two versions (above and below) are just a choice.
             drawer = new DrawMulti(new DrawRecipe(), new DrawDefault());
             
             ////// The library specific variables, and functions, all default values. You do not need to define all of them, only the ones you want to change.
@@ -38,67 +37,53 @@ public class ExampleBlocks {
             autoSelectRecipe = false;
             
             /// Adding a recipe
-            recipes.addAll(
-                // Multiple ways to create a recipe, the order is: name, input, output, craftTime.
-                // The letters 'A', 'B', 'C', 'D', 'E', and 'F' are just to differentiate the recipes, you can use any name you want.
-                new Recipe("empty-recipeA"),
-                new Recipe("empty-recipeB", new IOEntry()),
-                new Recipe("empty-recipeC", new IOEntry(), new IOEntry()),
-                new Recipe("empty-recipeD", new IOEntry(), new IOEntry(), 80f),
-                // You can use an anonymous class to define a recipe, like Mindustry blocks do. There's no recommended way, it depends on your preference.
-                // This is used to document all the available variables and functions, and their default values. You can change any of them.
-                // The 'J' prefix is just to differentiate the JSON empty recipe from the Java empty recipe, you can use any name you want.
-                new Recipe("Jempty-recipe") {{
-                    localizedName = "Empty Recipe";
-                    
+            // This is the new way to add a recipe, it is recommended over manipulating the recipes list directly.
+            addRecipe(
+                // Inputs and outputs are defined in the constructor, they do not have a builder setter.
+                // You can have an empty recipe, or only input by not defining any input or output.
+                // If you want no input but output, you can use 'in -> {}' which will be an empty input, or 'out -> {}' for an empty output.
+                new Recipe("recipe-example",
+                    in -> in.withItems().withLiquids().withPower(0).withHeat(0).withPayloads(),
+                    out -> out.withItems().withLiquids().withPower(0).withHeat(0).withPayloads(),
+                    80f
+                ).withLocalizedName("Example Recipe")
                     // When 'autoSelectRecipe' is true, this weight will influence which recipe will be selected in case of multiple valid recipes.
                     // The higher weighted recipe will be selected.
-                    weight = 1f;
-                    
+                    .withWeight(1f)
                     // The time, in tick, it takes to craft the recipe.
-                    craftTime = 80f;
-                    
-                    input.withItems(ItemStack.empty).withLiquids(LiquidStack.empty).withPower(0f).withHeat(0f).withPayloads(PayloadStack.with());
-                    output.withItems(ItemStack.empty).withLiquids(LiquidStack.empty).withPower(0f).withHeat(0f).withPayloads(PayloadStack.with());
-                    // Example for amount to amount/tick conversion:
-                    //     10 water/second = 10 / 60 = 0.167 water/tick
-                    //     0.83 water/tick = 0.83 * 60 = 50 water/second
-                    // It works for power or liquids quantity.
-                    
-                    craftEffect = Fx.none;
-                    updateEffect = Fx.none;
-                    updateEffectChance = 0.04f;
-                    updateEffectSpread = 4f;
-                    warmupSpeed = 0.019f;
-                    
+                    .withCraftTime(80f)
+                    .withCraftEffect(Fx.none)
+                    .withUpdateEffect(Fx.none, 0.04f, 4f)
+                    .withWarmupSpeed(0.019f)
                     // Only for recipes with heat
-                    warmupRate = 0.15f;
-                    overheatScale = 1f;
-                    maxEfficiency = 4f;
-                    
+                    .withWarmupRate(0.15f)
+                    .withOverheatScale(1f)
+                    .withMaxEfficiency(4f)
                     // If true, the crafter will output a random item from the output list, like a Separator/Disassembler.
                     // Only support items, all other output types will be forbidden and will throw an error if defined.
-                    randomOutput = false;
-                    
+                    .isRandomOutput(false)
                     // If false, the recipe will need to be researched to unlock. Useful for recipes that needs items unlocked later in your progression.
-                    unlocked = false;
+                    .isUnlocked(false)
                     // If true, the recipe will always be unlocked, even after a research tree reset.
-                    alwaysUnlocked = false;
-                    
+                    .isAlwaysUnlocked(false)
                     // If you have previously defined your recipe as unlocked and loaded the game, then later switched to locked,
                     //   the recipe will not automatically be locked. You will need to reset the research tree to lock it again, or lock this specific recipe using the console.
                     // To make a recipe locked, in the console: Vars.content.getByName(ContentType.typeid_UNUSED, "modname-recipename").clearUnlock()
-                    
+                    //
                     // For research, you need to define it inside a Tech Tree. See the 'TestTechTree' class for an example, or the game's vanilla tech tree for reference.
-                    
+                    //
                     // This drawer will be drawn at the place of a 'DrawRecipe' in the block drawer.
-                    drawer = new DrawDefault();
-                }}
-                // Recipes can also be defined using a builder setter style. There's no recommended way, it depends on your preference.
-                // They all start with 'with' or 'is' for booleans (they are not getters!), they allow you to chain them together to define a recipe variables.
-                // Inputs and outputs are defined in the constructor, they do not have a builder setter.
+                    .withDrawer(new DrawDefault())
             );
             
+            // This is the legacy way to add a recipe, it is still supported for backward compatibility, but it is not recommended. Use the 'addRecipe' function instead, seen above.
+            recipes.add(
+                // This is the deprecated way to add a recipe, it is still supported but not recommended.
+                // Use the callback constructor instead, as seen above.
+                new Recipe("deprecated-recipe-def", new IOEntry(), new IOEntry(), 80f)
+            );
+            
+            // Back to the default vanilla game variables, all values are examples.
             requirements(Category.crafting, ItemStack.with(Items.copper, 10));
         }};
         attributeExampleBlock = new AttributeMultiCrafterBlock("example-attribute-multi") {{
